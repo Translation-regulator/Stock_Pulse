9f3f7637a373c74b833c9adb01ca245e60a153a5
Conan <changchaocheng870405@gmail.com>
2025-05-12 14:55:20 +0800

    feat:Packge MySQL connection function as shared module

---
+++ backend/utils/db.py
@@ -0,0 +1,18 @@
+import os 
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    try:
+        con = mysql.connector.connect(
+            host = os.getenv("DB_HOST"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            database = os.getenv("DB_NAME"),
+            charset = "utf8mb4"
+        )
+    except mysql.connector.Error as e:
+        print(f"MySQL connection fail: {e}")
+        raise
